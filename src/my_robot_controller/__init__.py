acd9f21d971ec88506580d020a868653ab039873
Xiao Ming Tang <xmtang1@myseneca.ca>
2025-01-29 05:34:06 -0500

    Updated files

--- src/my_robot_controller/__init__.py
+++ src/my_robot_controller/__init__.py
@@ -3,14 +3,6 @@ import logging
 
 # Initialize logging for the package
 logging.basicConfig(level=logging.INFO)
-# Advanced: File handler with rotation
-file_handler = RotatingFileHandler('my_robot_controller.log', maxBytes=10240, backupCount=10)
-formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
-file_handler.setFormatter(formatter)
-
-# Logger setup
 logger = logging.getLogger(__name__)
-logger.addHandler(file_handler)
-logger.setLevel(logging.INFO)
 
 logger.info("Initializing my_robot_controller package...")
