b79d1cea0cc8bbe773da7b39ecbcd0d7c4e14627
Xiao Ming Tang <xmtang1@myseneca.ca>
2025-01-31 00:39:25 -0500

    updated files

--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
+import os
+from glob import glob
 package_name = 'my_robot_controller'
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=[package_name],  # This must match the inner `my_robot_controller/` directory
+    packages=find_packages(),  # Automatically finds the package inside src/my_robot_controller/
     data_files=[
         ('share/ament_index/resource_index/packages', ['package.xml']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Your Name',
-    maintainer_email='your-email@example.com',
+    maintainer='Xiao Ming Tang',
+    maintainer_email='xmtang1@myseneca.ca',
     description='A simple robot controller for ROS 2.',
     license='BSD',
     tests_require=['pytest'],
