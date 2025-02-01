d968dd7684136714d1947055f98f9cb6ac3ba082
Xiao Ming Tang <xmtang1@myseneca.ca>
2025-02-01 01:31:33 -0500

    updated files

--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup, find_packages
-import os
 from glob import glob
+import os
 
 package_name = 'my_robot_controller'
-current_dir = os.path.dirname(os.path.abspath(__file__))  # Get absolute path of setup.py
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(where='.'),  # Ensure package discovery in current directory
-    package_dir={'': '.'},  # Avoid using 'src', which causes build issues
-    options={
-        'build': {'build_base': 'build'},
-        # Removed egg_base option from install command
-    },    
+    packages=find_packages(where='.'),
+    package_dir={'': '.'},  # Packages are in the current folder
     data_files=[
-        ('share/ament_index/resource_index/packages', ['package.xml']),
+        # Install an empty marker file for ament.
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('lib/' + package_name, glob('my_robot_controller/*.py')),  # Fix package detection
+        ('lib/' + package_name, glob('my_robot_controller/*.py')),
         ('share/' + package_name, glob('launch/*.launch.py')),
         ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
