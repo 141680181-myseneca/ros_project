6bfb5ff5350d04d6034295a80694331359a48f0f
Xiao Ming Tang <xmtang1@myseneca.ca>
2025-02-01 01:14:47 -0500

    updated files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
 
-# Source the ROS 2 Humble environment instead of jazzy
+# Source the ROS 2 Humble environment instead of Jazzy
 source /opt/ros/humble/setup.bash
-# Source your workspace installation (if built successfully)
+# Source your workspace installation
 source /root/dev_ws/install/setup.bash
 
-# Execute the passed command (e.g., the default CMD)
+# Execute the passed command (default or override)
 exec "$@"
-
